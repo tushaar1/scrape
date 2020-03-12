6f437aaefde028a183237f3aee8852ed7c58ea60
tushaar <kaushik.tushaar@gmail.com>
2020-03-12 15:38:21 -0400

    - put info in a csv file(done) - scroll (already done) - show info in a clean way  (done)
    **ajax** fix needed, do more research and come back
    put rest is ok

--- sneakerrelease.py
+++ sneakerrelease.py
@@ -1,32 +1,39 @@
 from selenium import webdriver
 import time
 from selenium.webdriver.common.keys import Keys
-from urllib.request import urlopen as uReq
-from bs4 import BeautifulSoup as soup
+from bs4 import BeautifulSoup
+import pandas as pd
+import requests
 
 url = 'https://sneakernews.com/release-dates/'
 driver = webdriver.Chrome()
 driver.get(url)
+elem = driver.find_element_by_tag_name("body")
+# no_of_pagedowns = 350, to go to the bottom
+no_of_pagedowns = 20
 
-uClient = uReq(url)  # open up a connection and grabs the webpage
-page_html = uClient.read()  # puts the content into a variable
-uClient.close()  # closes the file
+while no_of_pagedowns:
+    elem.send_keys(Keys.PAGE_DOWN)
+    time.sleep(0.2)
+    no_of_pagedowns -= 1
+url = 'https://sneakernews.com/release-dates/'
+page = requests.get(url)
+data = page.text
+soup = BeautifulSoup(data, 'html.parser')
 
-results = []
-page_soup = soup(page_html, 'html.parser')  # parses the html into a new variable
+results = soup.find_all("div", attrs={'class': 'content-box'})
+print(len(results))
 
-'''release date'''
-for shoe in page_soup.find_all('span', {'class': 'release-date'}):
-    print(shoe.text)
+records = []
+for result in results:
+    date = result.find(class_='release-date').text.strip()
+    price = result.find(class_='release-price').text.strip()
+    hype = result.find(class_='release-rating').text.strip()
+    name = result.find('h2',{'a': ''}).text.strip()
+    records.append((name, price, date, hype))
+print(len(records))
 
-'''shoe name'''
-for shoe in page_soup.find_all('h2', {'a': ''}):
-    print(shoe.text)
-'''release price'''
-for shoe in page_soup.find_all('span', {'class': 'release-price'}):
-    print(shoe.text)
-
-'''release rating'''
-for shoe in page_soup.find_all('div', {'class': 'release-rating'}):
-    print(shoe.text)
-
+df = pd.DataFrame(records, columns=['name', 'price', 'release date', 'popularity'])
+print(df.head())
+print(df.tail())
+df.to_csv('sneaker_releases.csv', index=False, encoding='utf-8')
